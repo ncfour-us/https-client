6272b644d862b910feb3ac24ef05aa4336c24ab7
Tim Hahn <hahntj@gmail.com>
2024-04-27 07:25:28 -0400

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,14 @@
+import { TypeScriptESMProject } from '@ncfour/projen-utils';
+import { javascript } from 'projen';
+const project = new TypeScriptESMProject({
+  defaultReleaseBranch: 'main',
+  devDeps: ['@ncfour/projen-utils@file:../projen-utils/dist/js/projen-utils@0.0.0.jsii.tgz'],
+  name: 'http-client',
+  packageManager: javascript.NodePackageManager.NPM,
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
