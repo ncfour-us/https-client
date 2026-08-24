d139cc798130d3aa0e224d4d62135adf02aea7a4
Tim Hahn <hahntj@gmail.com>
2026-08-24 16:57:47 -0400

    feat: fill in example.ts with a useful example

--- examples/example.ts
+++ examples/example.ts
@@ -4,8 +4,39 @@
 // Run this using:
 //   node examples/lib/examples/example.js
 
-import { Hello } from '@ncfour-us/https-client';
+import { ILogger, createLogger } from '@ncfour-us/logging';
+import { HttpsClient, HttpsResponse } from '@ncfour-us/https-client';
 
-const myHello = new Hello();
+const myLogger: ILogger = createLogger('simple', {
+  json: false,
+  color: true,
+  level: 'trace',
+});
 
-myHello.sayHello();
+const myHttpClient = new HttpsClient(myLogger);
+
+try {
+  const response: HttpsResponse = await myHttpClient.request({
+    options: {
+      hostname: 'www.google.com',
+      port: 443,
+      path: '/',
+      method: 'GET',
+    },
+    body: '',
+  });
+
+  if (response?.metadata.statusCode === 200) {
+    myLogger.log(
+      'info',
+      `summary of response body: ${response.body.substring(0, 50)}...${response.body.substring(response.body.length - 50)}`,
+    );
+  } else {
+    myLogger.log(
+      'error',
+      `problem with request, response metadata:\n ${JSON.stringify(response.metadata, null, 2)}`,
+    );
+  }
+} catch (e) {
+  myLogger.log('error', `problem with request, e: ${JSON.stringify(e, null, 2)}`);
+}
